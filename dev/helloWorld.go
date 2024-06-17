eb3bf863db4579bfb1aab3ecc03f622e98ce5651
Aadarsh <chichsur@gmail.com>
2024-06-17 05:46:31 -0400

    testing out signining

--- dev/helloWorld.go
+++ dev/helloWorld.go
@@ -3,11 +3,14 @@ package main
 import (
 	"crypto/ecdh"
 	"crypto/ecdsa"
+	// "crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/x509"
+	"log"
 
 	// "encoding/json"
+	"encoding/hex"
 	"encoding/pem"
 	"fmt"
 )
@@ -15,9 +18,160 @@ import (
 func main() {
     fmt.Println("Hello World 123")
 
-    dhke()
+    // dhke()
     // pemToPubkey()
+
+    ecsign()
 }
+
+ // ECDSA signing for auth dhke ?
+
+ func ecsign(){
+
+    //gen key
+    // signPrivateKey,_ := ecdsa.GenerateKey(elliptic.P256(),rand.Reader)
+
+
+    // signPubKey := &signPrivateKey.PublicKey
+
+    // fmt.Println("[*] --- ECDSA KEYS --- ")
+
+    // prvStr,_ := EncodePrivate(signPrivateKey)
+    // pubStr,_ := EncodePublic(signPubKey)
+
+    // fmt.Printf("%v \n+-+-+-+-+-+\n%v",prvStr,pubStr)
+
+    // // Compute the SHA-256 hash
+    // hash := sha256.New()
+    // hash.Write([]byte("FEINFEINFEIN"))
+    // hashBytes := hash.Sum(nil)
+
+    // _=hashBytes
+
+    /*
+            -----BEGIN EC PRIVATE KEY-----
+        MHcCAQEEIOiFY1s/QDHKuLEW4vP/nP/G2c3ycmUa5nvU0+lrIZswoAoGCCqGSM49
+        AwEHoUQDQgAEbB6DTE8c36n4kugRSl7t9fkwmHZval42WatGQpCW3DL75oRjMsRX
+        48x03WrduU1XYWcRmjAY5RePhquNkI4gRw==
+        -----END EC PRIVATE KEY-----
+        
+        +-+-+-+-+-+
+        -----BEGIN PUBLIC KEY-----
+        MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEbB6DTE8c36n4kugRSl7t9fkwmHZv
+        al42WatGQpCW3DL75oRjMsRX48x03WrduU1XYWcRmjAY5RePhquNkI4gRw==
+        -----END PUBLIC KEY-----
+    */
+
+    var strprivkey = "-----BEGIN EC PRIVATE KEY-----\n"+
+    "MHcCAQEEIOiFY1s/QDHKuLEW4vP/nP/G2c3ycmUa5nvU0+lrIZswoAoGCCqGSM49"+
+    "AwEHoUQDQgAEbB6DTE8c36n4kugRSl7t9fkwmHZval42WatGQpCW3DL75oRjMsRX"+
+    "48x03WrduU1XYWcRmjAY5RePhquNkI4gRw==\n"+
+    "-----END EC PRIVATE KEY-----"
+
+    var strpubkey = "-----BEGIN PUBLIC KEY-----\n"+
+    "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEbB6DTE8c36n4kugRSl7t9fkwmHZv" + 
+    "al42WatGQpCW3DL75oRjMsRX48x03WrduU1XYWcRmjAY5RePhquNkI4gRw=="+
+    "\n-----END PUBLIC KEY-----"
+
+    private_key,err := DecodePrivateStr(strprivkey)
+    if err != nil {
+        log.Fatalf("FailDecode %v", err)
+    }
+    public_key, err := DecodePublicStr(strpubkey)
+    if err != nil {
+        log.Fatalf("Failed to Decode %v", err)
+    }
+
+    // fmt.Println(private_key,public_key)
+    // Compute the SHA-256 hash
+    hash := sha256.New()
+    hash.Write([]byte("FEINFEINFEIN"))
+    hashBytes := hash.Sum(nil)
+
+    ////////////////////////////////////////////
+
+    r,s, err := ecdsa.Sign(rand.Reader, private_key, hashBytes)
+    signature := append(r.Bytes(), s.Bytes()...)
+    if err != nil {
+        log.Fatalf("Failed to sign hash: %v", err)
+    }
+
+    // Print the ASN.1 encoded signature
+    fmt.Printf("R|S Signature: %s\n", hex.EncodeToString(signature))
+
+    // Verify the signature with the public key using VerifyASN1
+    valid := ecdsa.Verify(public_key, hashBytes[:], r,s)
+    if valid {
+        fmt.Println("Signature verification successful")
+    } else {
+        fmt.Println("Signature verification failed")
+    }
+
+
+
+ }
+
+
+ // Str -> Key object
+func DecodePrivateStr(pemEncodedPriv string) (privateKey *ecdsa.PrivateKey, err error) {
+	blockPriv, _ := pem.Decode([]byte(pemEncodedPriv))
+    
+    if blockPriv == nil {
+        fmt.Println("Failed to decode PEM block containing private key")
+    }
+	x509EncodedPriv := blockPriv.Bytes
+
+	privateKey, err = x509.ParseECPrivateKey(x509EncodedPriv)
+
+	return
+}
+
+// DecodePublic public key
+func DecodePublicStr(pemEncodedPub string) (publicKey *ecdsa.PublicKey, err error) {
+	blockPub, _ := pem.Decode([]byte(pemEncodedPub))
+    if blockPub == nil {
+        fmt.Println("Failed to decode PEM block containing private key")
+    }
+	x509EncodedPub := blockPub.Bytes
+
+	genericPublicKey, err := x509.ParsePKIXPublicKey(x509EncodedPub)
+	publicKey = genericPublicKey.(*ecdsa.PublicKey)
+
+	return
+}
+
+ // EncodePrivate private key
+func EncodePrivate(privKey *ecdsa.PrivateKey) (key string, err error) {
+
+    encoded, err := x509.MarshalECPrivateKey(privKey)
+
+    if err != nil {
+        return
+    }
+    pemEncoded := pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: encoded})
+
+    key = string(pemEncoded)
+
+    return
+}
+
+// EncodePublic public key
+func EncodePublic(pubKey *ecdsa.PublicKey) (key string, err error) {
+
+    encoded, err := x509.MarshalPKIXPublicKey(pubKey)
+
+    if err != nil {
+        return
+    }
+    pemEncodedPub := pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: encoded})
+
+    
+
+    key = string(pemEncodedPub)
+    return
+}
+
+
 
 // DIFFIE HELLMAN!
 
