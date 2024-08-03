4455e3b2de0ab40f072c53d673a5f995f7d9a02c
Aadarsh <chichsur@gmail.com>
2024-08-03 17:52:07 -0400

    cors url change needed

--- dev/testServer/testServer.go
+++ dev/testServer/testServer.go
@@ -387,7 +387,8 @@ func SGX_HMAC(secret string) (string, error) {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*") // i should use localhost:8080 instead
+        //actually use domain name edit hosts file ipaddr domain map
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
